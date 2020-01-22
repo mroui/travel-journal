ab852bc74bf1aa748f6ca9a5466128213362c7c0
Martyna Roj <martynaxroj@gmail.com>
2020-01-22 20:56:06 +0100

    [ADD] set adapter on viewpager & listener connect to navbar fragments

--- app/src/main/java/com/martynaroj/traveljournal/Activities/MainActivity.java
+++ app/src/main/java/com/martynaroj/traveljournal/Activities/MainActivity.java
@@ -1,28 +1,74 @@
 package com.martynaroj.traveljournal.Activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+import com.martynaroj.traveljournal.Adapters.NavigationBarAdapter;
+import com.martynaroj.traveljournal.Fragments.BoardFragment;
+import com.martynaroj.traveljournal.Fragments.HomeFragment;
+import com.martynaroj.traveljournal.Fragments.ProfileFragment;
 import com.martynaroj.traveljournal.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BubbleNavigationLinearView bottomNavigationBar;
-    private ViewPager fragmentsViewPager;
+    private BubbleNavigationLinearView navigation;
+    private ViewPager viewPager;
+    private NavigationBarAdapter adapter;
+    private List<Fragment> fragmentsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setViews();
+        findViews();
+        setListeners();
+        viewPager.setAdapter(adapter);
+        navigation.setTypeface(Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf"));
     }
 
-    private void setViews() {
-        bottomNavigationBar = findViewById(R.id.bottom_navigation_view);
-        fragmentsViewPager = findViewById(R.id.view_pager);
+
+    private void setListeners() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                navigation.setCurrentActiveItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+            }
+        });
+
+        navigation.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                viewPager.setCurrentItem(position, true);
+            }
+        });
     }
 
+
+    private void findViews() {
+        fragmentsList.add(0, new HomeFragment());
+        fragmentsList.add(1, new BoardFragment());
+        fragmentsList.add(2, new ProfileFragment());
+        navigation = findViewById(R.id.bottom_navigation_view);
+        viewPager = findViewById(R.id.view_pager);
+        adapter = new NavigationBarAdapter(fragmentsList, getSupportFragmentManager());
+    }
 
 }
