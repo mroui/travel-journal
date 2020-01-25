e12b5875226e876be6f5ce804a8fc58679476a64
Martyna Roj <martynaxroj@gmail.com>
2020-01-26 00:49:20 +0100

    [ADD] onBackPressed listener [MODIFY] add custom viewpager listener & nav listener with change fragment

--- app/src/main/java/com/martynaroj/traveljournal/Interfaces/NavigationListener.java
+++ app/src/main/java/com/martynaroj/traveljournal/Interfaces/NavigationListener.java
@@ -4,6 +4,6 @@ import androidx.fragment.app.Fragment;
 
 public interface NavigationListener {
 
-    void changeFragment(Fragment fragment, Boolean addToBackStack);
+    void changeFragment(Fragment previous, Fragment next, Boolean addToBackStack);
 
 }
--- app/src/main/java/com/martynaroj/traveljournal/MainActivity.java
+++ app/src/main/java/com/martynaroj/traveljournal/MainActivity.java
@@ -1,11 +1,14 @@
 package com.martynaroj.traveljournal;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
@@ -14,16 +17,19 @@ import com.martynaroj.traveljournal.Adapters.NavigationBarAdapter;
 import com.martynaroj.traveljournal.Fragments.BoardFragment;
 import com.martynaroj.traveljournal.Fragments.HomeFragment;
 import com.martynaroj.traveljournal.Fragments.ProfileFragment;
+import com.martynaroj.traveljournal.Interfaces.NavigationListener;
+import com.martynaroj.traveljournal.Others.ViewPagerListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationListener {
 
     private BubbleNavigationLinearView navigation;
     private ViewPager viewPager;
     private NavigationBarAdapter adapter;
     private List<Fragment> fragmentsList = new ArrayList<>();
+    private boolean backPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +44,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setListeners() {
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-            }
-
+        viewPager.addOnPageChangeListener(new ViewPagerListener() {
             @Override
             public void onPageSelected(int i) {
                 navigation.setCurrentActiveItem(i);
             }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-            }
         });
-
         navigation.setNavigationChangeListener(new BubbleNavigationChangeListener() {
             @Override
             public void onNavigationChanged(View view, int position) {
@@ -63,12 +60,43 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void findViews() {
-        fragmentsList.add(0, new HomeFragment());
-        fragmentsList.add(1, new BoardFragment());
-        fragmentsList.add(2, new ProfileFragment());
+        fragmentsList.add(0, HomeFragment.newInstance());
+        fragmentsList.add(1, BoardFragment.newInstance());
+        fragmentsList.add(2, ProfileFragment.newInstance());
         navigation = findViewById(R.id.bottom_navigation_view);
         viewPager = findViewById(R.id.view_pager);
         adapter = new NavigationBarAdapter(fragmentsList, getSupportFragmentManager());
     }
 
+
+    @Override
+    public void onBackPressed() {
+        if (adapter.getItem(navigation.getCurrentActiveItemPosition()).getChildFragmentManager().getBackStackEntryCount() >= 1) {
+            adapter.getItem(navigation.getCurrentActiveItemPosition()).getChildFragmentManager().popBackStack();
+            return;
+        }
+        if (backPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.backPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                backPressedOnce = false;
+            }
+        }, 2000);
+    }
+
+
+    @Override
+    public void changeFragment(Fragment previous, Fragment next, Boolean addToBackStack) {
+        if (previous.getView() != null) {
+            FragmentTransaction fragmentTransaction = previous.getChildFragmentManager().beginTransaction();
+            fragmentTransaction.replace(previous.getView().getId(), next);
+            if (addToBackStack) fragmentTransaction.addToBackStack(next.getTag());
+            fragmentTransaction.commit();
+        }
+    }
 }
