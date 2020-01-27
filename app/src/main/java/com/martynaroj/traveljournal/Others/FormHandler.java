355f007a545fb79abbd6544d786b5182055c250c
Martyna Roj <martynaxroj@gmail.com>
2020-01-27 15:59:43 +0100

    [ADD] validation for checking equality of inputs

--- app/src/main/java/com/martynaroj/traveljournal/Others/FormHandler.java
+++ app/src/main/java/com/martynaroj/traveljournal/Others/FormHandler.java
@@ -35,15 +35,35 @@ public class FormHandler {
         } else if (input.getInputType() == (InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS + InputType.TYPE_CLASS_TEXT) && !isValidEmail(value)) {
             layout.setError("Invalid email");
             input.requestFocus();
-        }
-        else
+            return false;
+        } else
             layout.setErrorEnabled(false);
+        return true;
+    }
+
+
+    public boolean validateInputsEquality(TextInputEditText input1, TextInputEditText input2, TextInputLayout layout2) {
+        if (!isEqual(input1, input2)) {
+            if(input1.getInputType() == InputType.TYPE_TEXT_VARIATION_PASSWORD + InputType.TYPE_CLASS_TEXT) {
+                layout2.setError("Password doesn't match");
+                input2.requestFocus();
+            }
+            return false;
+        } else
+            layout2.setErrorEnabled(false);
         return true;
     }
 
 
     private static boolean isValidEmail(String email) {
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+
+    private boolean isEqual(TextInputEditText input1, TextInputEditText input2) {
+        if (input1.getText() != null && input2.getText() != null)
+            return input1.getText().toString().equals(input2.getText().toString());
+        else return false;
     }
 
 
