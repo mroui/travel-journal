36023a21cb3ad26773e4fffd5bc04ff0c5b915e3
Martyna Roj <martynaxroj@gmail.com>
2020-02-06 20:02:21 +0100

    [MODIFY] fix readability of profile fragment style [ADD] expandable preferences

--- app/src/main/java/com/martynaroj/traveljournal/View/Fragments/ProfileFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/View/Fragments/ProfileFragment.java
@@ -1,19 +1,29 @@
 package com.martynaroj.traveljournal.View.Fragments;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.martynaroj.traveljournal.R;
 import com.martynaroj.traveljournal.View.Base.BaseFragment;
+import com.martynaroj.traveljournal.View.Others.Interfaces.Constants;
 import com.martynaroj.traveljournal.databinding.FragmentProfileBinding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class ProfileFragment extends BaseFragment implements View.OnClickListener {
@@ -31,6 +41,12 @@ public class ProfileFragment extends BaseFragment implements View.OnClickListene
         View view = binding.getRoot();
 
         setListeners();
+        List<String> names = new ArrayList<>(Arrays.asList("andfgna", "olaasdgfd", "ansdaasdna", "ola","anggna", "ola","asdfsdfnna", "ola", "dsfgsdfsdfs", "asfddsgdfg"));
+        binding.profilePreferences.setData(names, item -> {
+            SpannableString spannableString = new SpannableString("#" + item);
+            spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#000000")), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            return spannableString;
+        });
 
         return view;
     }
@@ -40,6 +56,7 @@ public class ProfileFragment extends BaseFragment implements View.OnClickListene
         binding.profileSignOutButton.setOnClickListener(this);
         binding.profileNotifications.setOnClickListener(this);
         binding.profileEdit.setOnClickListener(this);
+        binding.profileSeeAllPreferences.setOnClickListener(this);
     }
 
 
@@ -54,7 +71,26 @@ public class ProfileFragment extends BaseFragment implements View.OnClickListene
                 return;
             case R.id.profile_edit:
                 showSnackBar("clicked: edit", Snackbar.LENGTH_SHORT);
+                return;
+            case R.id.profile_see_all_preferences:
+                seeAllPreferences();
         }
+    }
+
+
+    private void seeAllPreferences() {
+        ConstraintLayout.LayoutParams constraintLayout = (ConstraintLayout.LayoutParams) binding.profilePreferences.getLayoutParams();
+        String seePreferences;
+        if (binding.profilePreferences.getLayoutParams().height == ConstraintLayout.LayoutParams.WRAP_CONTENT) {
+            constraintLayout.height = Constants.HASHTAG_HEIGHT;
+            seePreferences = getResources().getString(R.string.profile_see_all_pref);
+        } else {
+            constraintLayout.height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+            seePreferences = getResources().getString(R.string.profile_see_less_pref);
+        }
+        binding.profileSeeAllPreferences.setPaintFlags(binding.profileSeeAllPreferences.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        binding.profileSeeAllPreferences.setText(seePreferences);
+        binding.profilePreferences.setLayoutParams(constraintLayout);
     }
 
 
