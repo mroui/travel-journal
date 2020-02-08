8a226cf7b4563f944940c4498ee14834401933f0
Martyna Roj <martynaxroj@gmail.com>
2020-02-08 18:04:15 +0100

    [ADD] HashtagAdapter & list item style for nachos view with preferences

---
+++ app/src/main/java/com/martynaroj/traveljournal/view/adapters/HashtagAdapter.java
@@ -0,0 +1,91 @@
+package com.martynaroj.traveljournal.view.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.martynaroj.traveljournal.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashtagAdapter extends ArrayAdapter<String> {
+
+    private List<String> hashtags;
+
+    public HashtagAdapter(@NonNull Context context, @NonNull List<String> tags) {
+        super(context, 0, tags);
+        hashtags = new ArrayList<>(tags);
+    }
+
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return hashtagFilter;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        TextView textViewName = convertView.findViewById(R.id.list_item_name);
+        String item = getItem(position);
+        if (item != null) {
+            textViewName.setText(item);
+        }
+
+        return convertView;
+    }
+
+
+    private final Filter hashtagFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            List<String> suggestions = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                suggestions.addAll(hashtags);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (String item : hashtags) {
+                    if (item.toLowerCase().contains(filterPattern)) {
+                        suggestions.add(item);
+                    }
+                }
+            }
+            results.values = suggestions;
+            results.count = suggestions.size();
+
+            return results;
+        }
+
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            clear();
+            addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+
+
+        @Override
+        public CharSequence convertResultToString(Object resultValue) {
+            return "#" + resultValue;
+        }
+
+    };
+}
