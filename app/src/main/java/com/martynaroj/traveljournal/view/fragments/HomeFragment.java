fae1ff1f44e381037e5bf3f914d4f33f509c5fcb
Martyna Roj <martynaxroj@gmail.com>
2020-02-23 23:17:47 +0100

    [ADD] basic home screen style with title, img & viewpager for explore travels

--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/HomeFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/HomeFragment.java
@@ -6,10 +6,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.martynaroj.traveljournal.view.base.BaseFragment;
+import androidx.annotation.NonNull;
+
 import com.martynaroj.traveljournal.R;
+import com.martynaroj.traveljournal.databinding.FragmentHomeBinding;
+import com.martynaroj.traveljournal.services.models.Travel;
+import com.martynaroj.traveljournal.view.adapters.ExploreTravelsAdapter;
+import com.martynaroj.traveljournal.view.base.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends BaseFragment {
+
+    private FragmentHomeBinding binding;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -17,9 +27,32 @@ public class HomeFragment extends BaseFragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        initExploreTravelsAdapter();
+
         return view;
+    }
+
+
+    private void initExploreTravelsAdapter() {
+        List<Travel> travels = new ArrayList<>();
+        travels.add(new Travel(R.drawable.ic_avatar, "title", "desc"));
+        travels.add(new Travel(R.drawable.ic_avatar, "title", "desc"));
+        travels.add(new Travel(R.drawable.ic_avatar, "title", "desc"));
+
+        ExploreTravelsAdapter adapter = new ExploreTravelsAdapter(travels, getContext());
+        binding.homeExploreViewpager.setAdapter(adapter);
+        binding.homeExploreViewpager.setPadding(50, 0, 50, 0);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
