4f9a2cdfa3e3ccc95fca9c1c856ad623562b5332
Martyna Roj <martynaxroj@gmail.com>
2020-02-26 21:00:18 +0100

    [ADD] search view listener helper class

---
+++ app/src/main/java/com/martynaroj/traveljournal/view/others/classes/SearchViewListener.java
@@ -0,0 +1,16 @@
+package com.martynaroj.traveljournal.view.others.classes;
+
+
+import androidx.appcompat.widget.SearchView;
+
+public class SearchViewListener implements SearchView.OnQueryTextListener {
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
+    }
+}
