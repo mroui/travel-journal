ba32dad42a9a67e762864b44b9436878e5c08c2a
Martyna Roj <martynaxroj@gmail.com>
2020-02-27 21:34:51 +0100

    [ADD] recycler view, user adapter, searching by query on text changes, no results string

--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/SearchFriendsFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/SearchFriendsFragment.java
@@ -4,16 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.martynaroj.traveljournal.R;
 import com.martynaroj.traveljournal.databinding.FragmentSearchFriendsBinding;
+import com.martynaroj.traveljournal.services.models.User;
+import com.martynaroj.traveljournal.view.adapters.UserAdapter;
 import com.martynaroj.traveljournal.view.base.BaseFragment;
+import com.martynaroj.traveljournal.view.others.classes.SearchViewListener;
+import com.martynaroj.traveljournal.view.others.interfaces.Constants;
 
 public class SearchFriendsFragment extends BaseFragment implements View.OnClickListener {
 
     private FragmentSearchFriendsBinding binding;
+    private CollectionReference usersRef;
+    private UserAdapter adapter;
 
     public static SearchFriendsFragment newInstance() {
         return new SearchFriendsFragment();
@@ -25,14 +36,65 @@ public class SearchFriendsFragment extends BaseFragment implements View.OnClickL
         binding = FragmentSearchFriendsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        initUsersReference();
         setListeners();
+        focusOnSearchView();
 
         return view;
     }
 
 
+    private void initUsersReference() {
+        usersRef = FirebaseFirestore.getInstance().collection(Constants.USERS);
+    }
+
+
     private void setListeners() {
         binding.searchFriendsArrowButton.setOnClickListener(this);
+        binding.searchFriendsSearchView.setOnQueryTextListener(new SearchViewListener() {
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (!s.equals("")) {
+                    searchUser(s);
+                } else {
+                    binding.searchFriendsRecyclerView.setAdapter(null);
+                    adapter.stopListening();
+                    binding.searchFriendsMessage.setVisibility(View.GONE);
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void focusOnSearchView() {
+        binding.searchFriendsSearchView.requestFocus();
+    }
+
+
+    private void searchUser(String username) {
+        Query query = usersRef.orderBy(Constants.USERNAME).startAt(username).endAt(username + "\uf8ff");
+        FirestoreRecyclerOptions<User> options = new FirestoreRecyclerOptions.Builder<User>()
+                .setLifecycleOwner(getViewLifecycleOwner())
+                .setQuery(query, User.class)
+                .build();
+        adapter = new UserAdapter(options, getContext());
+        binding.searchFriendsRecyclerView.swapAdapter(adapter, true);
+        adapter.startListening();
+        adapter.setOnItemClickListener((documentSnapshot, position) -> {
+            User user = documentSnapshot.toObject(User.class);
+            if (user != null) {
+                Toast.makeText(getContext(),
+                        "Clicked: " + user.getUid(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        //TODO: query async refactor mvvm: pagination + recyclerview + limit(10) + refresh on swipe + onclick + messages when no results / start to search
+        if (adapter.getItemCount() == 0) {
+            binding.searchFriendsMessage.setVisibility(View.VISIBLE);
+            //binding.searchFriendsMessage.setText(getResources().getString(R.string.search_friends_no_results));
+        } else {
+            binding.searchFriendsMessage.setVisibility(View.GONE);
+        }
     }
 
 
@@ -43,6 +105,14 @@ public class SearchFriendsFragment extends BaseFragment implements View.OnClickL
                 if (getParentFragmentManager().getBackStackEntryCount() > 0)
                     getParentFragmentManager().popBackStack();
         }
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (adapter != null)
+            adapter.stopListening();
     }
 
 
