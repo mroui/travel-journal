82aed5c3b5612698c225d376d9ec6c56ef4145cb
Martyna Roj <martynaxroj@gmail.com>
2020-03-05 15:54:49 +0100

    [ADD] friends list recycler view with new user adapter & getting list from database

---
+++ app/src/main/java/com/martynaroj/traveljournal/view/adapters/UserAdapter.java
@@ -0,0 +1,92 @@
+package com.martynaroj.traveljournal.view.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.martynaroj.traveljournal.R;
+import com.martynaroj.traveljournal.databinding.ItemUserBinding;
+import com.martynaroj.traveljournal.services.models.User;
+import com.martynaroj.traveljournal.view.interfaces.OnItemClickListener;
+import com.martynaroj.traveljournal.view.others.enums.Privacy;
+
+import java.util.List;
+import java.util.Objects;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
+
+    private Context context;
+    private List<User> users;
+    private OnItemClickListener listener;
+
+
+    public UserAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemUserBinding binding = ItemUserBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new UserHolder(binding);
+    }
+
+
+    private User getItem(int position) {
+        return this.users.get(position);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+        final User user = users.get(position);
+        holder.binding.userItemUsername.setText(user.getUsername());
+        if (user.getPrivacyEmail() == Privacy.PUBLIC.ordinal()) {
+            holder.binding.userItemEmail.setText(user.getEmail());
+        } else {
+            holder.binding.userItemEmail.setVisibility(View.GONE);
+        }
+        Glide.with(context)
+                .load(user.getPhoto())
+                .placeholder(R.drawable.default_avatar)
+                .into(holder.binding.userItemImage);
+        holder.binding.userItem.setOnClickListener(view -> listener.onItemClick(
+                Objects.requireNonNull(getItem(position)),
+                position,
+                holder.binding.userItem));
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.listener = onItemClickListener;
+    }
+
+
+    public void remove (int position) {
+        users.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+
+    static class UserHolder extends RecyclerView.ViewHolder {
+        private ItemUserBinding binding;
+        UserHolder(ItemUserBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/FriendsListFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/FriendsListFragment.java
@@ -6,17 +6,40 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.martynaroj.traveljournal.R;
 import com.martynaroj.traveljournal.databinding.FragmentFriendsListBinding;
+import com.martynaroj.traveljournal.services.models.User;
+import com.martynaroj.traveljournal.view.adapters.UserAdapter;
 import com.martynaroj.traveljournal.view.base.BaseFragment;
+import com.martynaroj.traveljournal.viewmodels.UserViewModel;
+
+import java.util.List;
 
 public class FriendsListFragment extends BaseFragment implements View.OnClickListener {
 
     private FragmentFriendsListBinding binding;
+    private UserViewModel userViewModel;
+    private User user;
+    private User loggedUser;
 
-    static FriendsListFragment newInstance() {
-        return new FriendsListFragment();
+
+    static FriendsListFragment newInstance(User user) {
+        return new FriendsListFragment(user);
+    }
+
+
+    public FriendsListFragment() {
+        super();
+    }
+
+
+    private FriendsListFragment(User user) {
+        super();
+        this.user = user;
     }
 
 
@@ -25,14 +48,97 @@ public class FriendsListFragment extends BaseFragment implements View.OnClickLis
         binding = FragmentFriendsListBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        initViewModels();
         setListeners();
+
+        initUser();
+        initLoggedUser();
+
+        initFriends();
 
         return view;
     }
 
 
+    private void initViewModels() {
+        if (getActivity() != null) {
+            userViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
+        }
+    }
+
+
     private void setListeners() {
         binding.friendsListArrowButton.setOnClickListener(this);
+    }
+
+
+    private void initUser() {
+        if (user == null) {
+            if (getParentFragmentManager().getBackStackEntryCount() > 0) {
+                getParentFragmentManager().popBackStack();
+            }
+        }
+    }
+
+
+    private void initLoggedUser() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            startProgressBar();
+            userViewModel.getUserData(firebaseAuth.getCurrentUser().getUid());
+            userViewModel.getUserLiveData().observe(getViewLifecycleOwner(), user -> {
+                if (user != null) {
+                    this.loggedUser = user;
+                } else {
+                    showSnackBar(getResources().getString(R.string.messages_error_current_user_not_available), Snackbar.LENGTH_LONG);
+                }
+                stopProgressBar();
+            });
+        }
+    }
+
+
+    private void initFriends() {
+        if (user != null && user.getFriends() != null && !user.getFriends().isEmpty()) {
+            startProgressBar();
+            binding.friendsListMessage.setVisibility(View.INVISIBLE);
+            userViewModel.getUsersListData(user.getFriends());
+            userViewModel.getUsersList().observe(getViewLifecycleOwner(), users -> {
+                if (users != null) {
+                    initFriendsList(users);
+                    stopProgressBar();
+                }
+            });
+        } else {
+            binding.friendsListRecyclerView.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+    private void initFriendsList(List<User> friends) {
+        UserAdapter adapter = new UserAdapter(getContext(), friends);
+        binding.friendsListRecyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener((object, position, view) -> {
+            User userItem = (User) object;
+            if (userItem != null) {
+                switch (view.getId()) {
+                    case R.id.user_item:
+                        if (loggedUser != null && loggedUser.isUserProfile(userItem))
+                            showSnackBar(getResources().getString(R.string.messages_its_you), Snackbar.LENGTH_SHORT);
+                        else
+                            changeFragment(ProfileFragment.newInstance(userItem));
+                        break;
+                }
+            }
+        });
+    }
+
+
+    private void changeFragment(BaseFragment next) {
+        if (user.isUserProfile(loggedUser))
+            getNavigationInteractions().changeFragment(this, next, true);
+        else
+            getNavigationInteractions().changeFragment(getParentFragment(), next, true);
     }
 
 
@@ -43,6 +149,21 @@ public class FriendsListFragment extends BaseFragment implements View.OnClickLis
                 if (getParentFragmentManager().getBackStackEntryCount() > 0)
                     getParentFragmentManager().popBackStack();
         }
+    }
+
+
+    private void startProgressBar() {
+        getProgressBarInteractions().startProgressBar(binding.getRoot(), binding.friendsListProgressbarLayout, binding.friendsListProgressbar);
+    }
+
+
+    private void stopProgressBar() {
+        getProgressBarInteractions().stopProgressBar(binding.getRoot(), binding.friendsListProgressbarLayout, binding.friendsListProgressbar);
+    }
+
+
+    private void showSnackBar(String message, int duration) {
+        getSnackBarInteractions().showSnackBar(binding.getRoot(), getActivity(), message, duration);
     }
 
 
