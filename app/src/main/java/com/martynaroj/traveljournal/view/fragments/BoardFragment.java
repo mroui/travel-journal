ec784fbfacd82463c3b068ff0dd1c688055e5004
Martyna Roj <martynaxroj@gmail.com>
2020-03-09 20:39:45 +0100

    [ADD] board fragment with basic layout & initializing view and user

--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/BoardFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/BoardFragment.java
@@ -1,15 +1,32 @@
 package com.martynaroj.traveljournal.view.fragments;
 
 
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.martynaroj.traveljournal.view.base.BaseFragment;
-import com.martynaroj.traveljournal.R;
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.lifecycle.ViewModelProvider;
 
-public class BoardFragment extends BaseFragment {
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.martynaroj.traveljournal.R;
+import com.martynaroj.traveljournal.databinding.FragmentBoardBinding;
+import com.martynaroj.traveljournal.services.models.User;
+import com.martynaroj.traveljournal.view.base.BaseFragment;
+import com.martynaroj.traveljournal.view.others.interfaces.Constants;
+import com.martynaroj.traveljournal.viewmodels.UserViewModel;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+
+public class BoardFragment extends BaseFragment implements View.OnClickListener {
+
+    private FragmentBoardBinding binding;
+    private UserViewModel userViewModel;
+    private User user;
 
     public static BoardFragment newInstance() {
         return new BoardFragment();
@@ -17,9 +34,113 @@ public class BoardFragment extends BaseFragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_board, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentBoardBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        initViewModels();
+        initLoggedUser();
+        initFloatingMenu();
+
+        observeUserChanges();
+
+        setListeners();
+
         return view;
+    }
+
+
+    private void initViewModels() {
+        if (getActivity() != null) {
+            userViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
+        }
+    }
+
+
+    private void initLoggedUser() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            startProgressBar();
+            userViewModel.getUserData(firebaseAuth.getCurrentUser().getUid());
+            userViewModel.getUserLiveData().observe(getViewLifecycleOwner(), user -> {
+                if (user != null) {
+                    this.user = user;
+                } else {
+                    showSnackBar(getResources().getString(R.string.messages_error_current_user_not_available), Snackbar.LENGTH_LONG);
+                }
+                stopProgressBar();
+            });
+        }
+    }
+
+
+    private void initFloatingMenu() {
+        binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_map_white, Constants.MAP,
+                getResources().getColor(R.color.main_red)));
+        binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_weather_white, Constants.WEATHER,
+                getResources().getColor(R.color.main_yellow)));
+        binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_money_white, Constants.CURRENCY,
+                getResources().getColor(R.color.main_green)));
+        binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_translator_white, Constants.TRANSLATOR,
+                getResources().getColor(R.color.main_blue)));
+    }
+
+
+    private void observeUserChanges() {
+        userViewModel.getUser().observe(getViewLifecycleOwner(), user -> {
+            if (user != null && user.isUserProfile(this.user)) {
+                this.user = user;
+                //binding.setUser(user);
+            }
+        });
+    }
+
+
+    private TextInsideCircleButton.Builder createMenuItem(int icon, String text, int color) {
+        if (getContext() != null) {
+            return new TextInsideCircleButton.Builder()
+                    .normalImageRes(icon)
+                    .imagePadding(new Rect(30, 30, 30, 50))
+                    .normalText(text)
+                    .typeface(ResourcesCompat.getFont(getContext(), R.font.raleway_bold))
+                    .textSize(8)
+                    .normalColor(color)
+                    .highlightedColor(Color.WHITE)
+                    .rippleEffect(true);
+        } else return null;
+    }
+
+
+    private void setListeners() {
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+        }
+    }
+
+
+    private void startProgressBar() {
+        getProgressBarInteractions().startProgressBar(binding.getRoot(), binding.boardProgressbarLayout, binding.boardProgressbar);
+    }
+
+
+    private void stopProgressBar() {
+        getProgressBarInteractions().stopProgressBar(binding.getRoot(), binding.boardProgressbarLayout, binding.boardProgressbar);
+    }
+
+
+    private void showSnackBar(String message, int duration) {
+        getSnackBarInteractions().showSnackBar(binding.getRoot(), getActivity(), message, duration);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
