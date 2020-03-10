fe061a49722ab6d199dfbd8f096b4652a9e7a57a
Martyna Roj <martynaxroj@gmail.com>
2020-03-10 17:43:20 +0100

    [ADD] alarm fragment to floating menu & onclick listener with changing fragments

--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/BoardFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/BoardFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -84,6 +85,8 @@ public class BoardFragment extends BaseFragment implements View.OnClickListener 
                 getResources().getColor(R.color.main_green)));
         binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_translator_white, Constants.TRANSLATOR,
                 getResources().getColor(R.color.main_blue)));
+        binding.boardFloatingMenuButton.addBuilder(createMenuItem(R.drawable.ic_alarm_white, Constants.ALARM,
+                getResources().getColor(R.color.main_violet)));
     }
 
 
@@ -105,19 +108,59 @@ public class BoardFragment extends BaseFragment implements View.OnClickListener 
                     .textSize(8)
                     .normalColor(color)
                     .highlightedColor(Color.WHITE)
-                    .rippleEffect(true);
+                    .rippleEffect(true)
+                    .listener(index -> {
+                        if(user != null) {
+                            switch (index) {
+                                case 0:
+                                    changeFragment(MapFragment.newInstance());
+                                    break;
+                                case 1:
+                                    changeFragment(WeatherFragment.newInstance());
+                                    break;
+                                case 2:
+                                    changeFragment(CurrencyFragment.newInstance());
+                                    break;
+                                case 3:
+                                    changeFragment(TranslateFragment.newInstance());
+                                    break;
+                                case 4:
+                                    changeFragment(AlarmFragment.newInstance());
+                            }
+                        } else {
+                            showSnackBar(getResources().getString(R.string.messages_not_logged_user), Snackbar.LENGTH_LONG);
+                        }
+                    });
         } else return null;
     }
 
 
     private void setListeners() {
+        binding.boardNewJourneyButton.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.board_new_journey_button:
+                startNewJourney();
+                break;
         }
+    }
+
+
+    private void startNewJourney() {
+        if (user != null) {
+
+        } else {
+            showSnackBar(getResources().getString(R.string.messages_not_logged_user), Snackbar.LENGTH_LONG);
+        }
+    }
+
+
+    private void changeFragment(Fragment next) {
+        getNavigationInteractions().changeFragment(this, next, true);
     }
 
 
