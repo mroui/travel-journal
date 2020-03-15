c22d74f280112632ca34cb327522c32a43f5af44
Martyna Roj <martynaxroj@gmail.com>
2020-03-15 19:17:21 +0100

    [ADD] view model & repo for rest api call for nearby places

---
+++ app/src/main/java/com/martynaroj/traveljournal/services/respositories/PlaceRepository.java
@@ -0,0 +1,49 @@
+package com.martynaroj.traveljournal.services.respositories;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.martynaroj.traveljournal.R;
+import com.martynaroj.traveljournal.services.models.PlacesAPI.PlacesResult;
+import com.martynaroj.traveljournal.services.retrofit.Rest;
+import com.martynaroj.traveljournal.view.others.interfaces.Constants;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PlaceRepository {
+
+    private Context context;
+
+    public PlaceRepository(Context context) {
+        this.context = context;
+    }
+
+    public MutableLiveData<PlacesResult> getPlacesResult(LatLng latLng, String type) {
+        MutableLiveData<PlacesResult> placesResultData = new MutableLiveData<>();
+        Rest.getRest().getNearbyPlaces(
+                latLng.latitude + "," + latLng.longitude,
+                Constants.NEARBY_PLACES_RADIUS,
+                type,
+                context.getString(R.string.google_api_key)
+        ).enqueue(new Callback<PlacesResult>() {
+            @Override
+            public void onResponse(@NonNull Call<PlacesResult> call, @NonNull Response<PlacesResult> response) {
+                if (response.isSuccessful()) {
+                    placesResultData.setValue(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<PlacesResult> call, @NonNull Throwable t) {
+                placesResultData.setValue(null);
+            }
+        });
+        return placesResultData;
+    }
+
+}
---
+++ app/src/main/java/com/martynaroj/traveljournal/viewmodels/PlaceViewModel.java
@@ -0,0 +1,31 @@
+package com.martynaroj.traveljournal.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.martynaroj.traveljournal.services.models.PlacesAPI.PlacesResult;
+import com.martynaroj.traveljournal.services.respositories.PlaceRepository;
+
+public class PlaceViewModel extends AndroidViewModel {
+
+    private LiveData<PlacesResult> placesResultLiveData;
+    private PlaceRepository placeRepository;
+
+    public PlaceViewModel(@NonNull Application application) {
+        super(application);
+        placeRepository = new PlaceRepository(application.getApplicationContext());
+    }
+
+    public void getPlaces(LatLng latLng, String type) {
+        placesResultLiveData = placeRepository.getPlacesResult(latLng, type);
+    }
+
+    public LiveData<PlacesResult> getPlacesResultData() {
+        return placesResultLiveData;
+    }
+
+}
