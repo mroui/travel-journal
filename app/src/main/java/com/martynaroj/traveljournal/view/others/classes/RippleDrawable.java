7052501365b11bead0159ed9c58293374c3a4db5
Martyna Roj <martynaxroj@gmail.com>
2020-03-16 19:31:25 +0100

    [ADD] ripple drawable helper class to set ripple effects on buttons with custom colors

---
+++ app/src/main/java/com/martynaroj/traveljournal/view/others/classes/RippleDrawable.java
@@ -0,0 +1,34 @@
+package com.martynaroj.traveljournal.view.others.classes;
+
+import android.content.res.ColorStateList;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+
+import com.google.android.material.button.MaterialButton;
+
+public abstract class RippleDrawable {
+
+
+    public static void setRippleEffectButton(MaterialButton button, int normalColor, int pressedColor) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            button.setRippleColor(new ColorStateList(new int[][]{ new int[]{}},new int[] {pressedColor}));
+        else
+            button.setBackgroundDrawable(getStateListDrawable(normalColor, pressedColor));
+    }
+
+
+    private static StateListDrawable getStateListDrawable(int normalColor, int pressedColor) {
+        StateListDrawable states = new StateListDrawable();
+        states.addState(new int[]{android.R.attr.state_pressed},
+                new ColorDrawable(pressedColor));
+        states.addState(new int[]{android.R.attr.state_focused},
+                new ColorDrawable(pressedColor));
+        states.addState(new int[]{android.R.attr.state_activated},
+                new ColorDrawable(pressedColor));
+        states.addState(new int[]{},
+                new ColorDrawable(normalColor));
+        return states;
+    }
+
+}
