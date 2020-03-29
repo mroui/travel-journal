912aa5b0a240d3afea8ea073e027967e184177dc
Martyna Roj <martynaxroj@gmail.com>
2020-03-29 22:16:43 +0200

    [ADD] generating id, adding, getting & updating methods to travel repository

--- app/src/main/java/com/martynaroj/traveljournal/services/respositories/TravelRepository.java
+++ app/src/main/java/com/martynaroj/traveljournal/services/respositories/TravelRepository.java
@@ -2,20 +2,73 @@ package com.martynaroj.traveljournal.services.respositories;
 
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.martynaroj.traveljournal.services.models.Travel;
+import com.martynaroj.traveljournal.view.others.enums.Status;
+import com.martynaroj.traveljournal.view.others.interfaces.Constants;
+
+import java.util.Map;
 
 public class TravelRepository {
 
     private Context context;
+    private CollectionReference travelsRef;
 
     private TravelRepository() {
         FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+        travelsRef = rootRef.collection(Constants.TRAVELS);
     }
 
 
     public TravelRepository(Context context) {
         this();
         this.context = context;
+    }
+
+
+    public String generateId() {
+        return travelsRef.document().getId();
+    }
+
+
+    public MutableLiveData<Status> addTravel(Travel travel) {
+        MutableLiveData<Status> status = new MutableLiveData<>();
+        DocumentReference travelRef = travelsRef.document(travel.getId());
+        travelRef.set(travel).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                status.setValue(Status.SUCCESS);
+            } else
+                status.setValue(Status.ERROR);
+        });
+        return status;
+    }
+
+
+    public void updateTravel(String id, Map<String, Object> changes) {
+        DocumentReference travelRef = travelsRef.document(id);
+        travelRef.update(changes);
+    }
+
+
+    public LiveData<Travel> getTravel(String id) {
+        MutableLiveData<Travel> travelData = new MutableLiveData<>();
+        DocumentReference travelRef = travelsRef.document(id);
+        travelRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document != null && document.exists()) {
+                    Travel travel = document.toObject(Travel.class);
+                    travelData.setValue(travel);
+                }
+            }
+        });
+        return travelData;
     }
 
 }
--- app/src/main/java/com/martynaroj/traveljournal/viewmodels/TravelViewModel.java
+++ app/src/main/java/com/martynaroj/traveljournal/viewmodels/TravelViewModel.java
@@ -8,15 +8,40 @@ import androidx.lifecycle.LiveData;
 
 import com.martynaroj.traveljournal.services.models.Travel;
 import com.martynaroj.traveljournal.services.respositories.TravelRepository;
+import com.martynaroj.traveljournal.view.others.enums.Status;
+
+import java.util.Map;
 
 public class TravelViewModel extends AndroidViewModel {
 
-    private LiveData<Travel> travelLiveData;
     private TravelRepository travelRepository;
+    private LiveData<Status> statusLiveData;
+    private LiveData<Travel> travelLiveData;
 
     public TravelViewModel(@NonNull Application application) {
         super(application);
         travelRepository = new TravelRepository(application.getApplicationContext());
+    }
+
+    public String generateId() {
+        return travelRepository.generateId();
+    }
+
+    public void addTravel(Travel travel) {
+        statusLiveData = travelRepository.addTravel(travel);
+    }
+
+    public LiveData<Status> getStatusData() {
+        return statusLiveData;
+    }
+
+    public void updateTravel(String id, Map<String, Object> changes) {
+        travelRepository.updateTravel(id, changes);
+        getTravel(id);
+    }
+
+    public void getTravel(String id) {
+        travelLiveData = travelRepository.getTravel(id);
     }
 
     public LiveData<Travel> getTravelData() {
