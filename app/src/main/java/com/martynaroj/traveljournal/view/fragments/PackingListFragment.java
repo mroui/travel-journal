c8e5d35a2911c01bdfef0bed10813813cc2cb1a4
Martyna Roj <martynaxroj@gmail.com>
2020-04-01 13:51:27 +0200

    [ADD] observing user & travel changes, loading travel & fill packing list data

--- app/src/main/java/com/martynaroj/traveljournal/view/fragments/PackingListFragment.java
+++ app/src/main/java/com/martynaroj/traveljournal/view/fragments/PackingListFragment.java
@@ -7,15 +7,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.martynaroj.traveljournal.R;
 import com.martynaroj.traveljournal.databinding.FragmentPackingListBinding;
 import com.martynaroj.traveljournal.services.models.Travel;
+import com.martynaroj.traveljournal.services.models.packing.PackingCategory;
+import com.martynaroj.traveljournal.services.models.packing.PackingItem;
+import com.martynaroj.traveljournal.view.adapters.PackingAdapter;
 import com.martynaroj.traveljournal.view.base.BaseFragment;
+import com.martynaroj.traveljournal.viewmodels.TravelViewModel;
+import com.martynaroj.traveljournal.viewmodels.UserViewModel;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PackingListFragment extends BaseFragment implements View.OnClickListener {
 
     private FragmentPackingListBinding binding;
+    private UserViewModel userViewModel;
+    private TravelViewModel travelViewModel;
     private Travel travel;
 
     private PackingListFragment(Travel travel) {
@@ -33,6 +46,13 @@ public class PackingListFragment extends BaseFragment implements View.OnClickLis
         binding = FragmentPackingListBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        initViewModels();
+
+        observeUserChanges();
+        observeTravelChanges();
+
+        initListData();
+
         setListeners();
 
         return view;
@@ -42,7 +62,60 @@ public class PackingListFragment extends BaseFragment implements View.OnClickLis
     //INIT DATA-------------------------------------------------------------------------------------
 
 
-    //
+    private void initViewModels() {
+        if (getActivity() != null) {
+            userViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
+            travelViewModel = new ViewModelProvider(getActivity()).get(TravelViewModel.class);
+        }
+    }
+
+
+    private void observeUserChanges() {
+        userViewModel.getUser().observe(getViewLifecycleOwner(), user -> {
+            if (user == null) {
+                back();
+            } else if (user.getActiveTravelId() != null && !user.getActiveTravelId().equals(""))
+                loadTravel(user.getActiveTravelId());
+            else
+                travelViewModel.setTravel(null);
+        });
+    }
+
+
+    private void observeTravelChanges() {
+        travelViewModel.getTravel().observe(getViewLifecycleOwner(), travel -> {
+            if(travel == null) {
+                showSnackBar(getResources().getString(R.string.messages_error_failed_load_travel),
+                        Snackbar.LENGTH_LONG);
+                back();
+            }
+            this.travel = travel;
+            stopProgressBar();
+        });
+    }
+
+
+    private void loadTravel(String id) {
+        startProgressBar();
+        travelViewModel.getTravelData(id);
+        travelViewModel.getTravelLiveData().observe(getViewLifecycleOwner(), travel ->
+                travelViewModel.setTravel(travel)
+        );
+    }
+
+
+    private void initListData() {
+        if (travel != null) {
+            Map<PackingCategory, List<PackingItem>> items = new HashMap<>();
+            for(PackingCategory category : travel.getPackingList())
+                items.put(category, category.getItems());
+            binding.packingListExpandableList.setAdapter(new PackingAdapter(
+                    getContext(),
+                    travel.getPackingList(),
+                    items
+            ));
+        }
+    }
 
 
     //LISTENERS-------------------------------------------------------------------------------------
@@ -58,6 +131,8 @@ public class PackingListFragment extends BaseFragment implements View.OnClickLis
         switch (v.getId()) {
             case R.id.packing_list_arrow_button:
                 back();
+                break;
+            case R.id.packing_list_add_button:
                 break;
         }
     }
