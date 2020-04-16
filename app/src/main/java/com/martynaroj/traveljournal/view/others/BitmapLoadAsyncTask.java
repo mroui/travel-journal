454d1f6989b484c5aea257e3e1d737baf2bf2438
Martyna Roj <martynaxroj@gmail.com>
2020-04-16 22:14:22 +0200

    [ADD] bitmap load async task helper class to create bitmap from url in background

---
+++ app/src/main/java/com/martynaroj/traveljournal/view/others/BitmapLoadAsyncTask.java
@@ -0,0 +1,36 @@
+package com.martynaroj.traveljournal.view.others;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class BitmapLoadAsyncTask extends AsyncTask<Void, Void, Bitmap> {
+
+    private String url;
+
+
+    public BitmapLoadAsyncTask(String url) {
+        this.url = url;
+    }
+
+
+    @Override
+    protected Bitmap doInBackground(Void... params) {
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(new URL(url).openStream());
+        } catch (IOException ignored) {
+        }
+        return bitmap;
+    }
+
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+        super.onPostExecute(result);
+    }
+
+}
